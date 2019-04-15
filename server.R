9db9856f749d7af2383690173ab2c70d2a144fd0
Brody Moore <showmesuccess@hotmail.com>
2019-04-15 17:36:21 -0600

    Making minor adjustments

--- server.R
+++ server.R
@@ -15,12 +15,12 @@ function(input, output){
   output$predictions <- DT::renderDataTable({
     data.frame(
       iris = c("setosa", "versicolor", "virginica"),
-      Probs = predict(IrisModel, as.matrix(NP()))
+      Probs = sort(predict(IrisModel, as.matrix(NP())),decreasing = TRUE)
     )
   })
   output$scatter <- renderPlot({
       plot(x =iris$Sepal.Length, y = iris$Sepal.Width, xlab ="Sepal Length", ylab = "Sepal Width", main = "Sepal Length-Width Scatter Plot")
-      points(newPredict(), col="red", pch =20)
+      points(NP(), col="red", pch =20)
   })
   output$den1 <- renderPlot({
     d1 <- density(iris$Sepal.Width)
@@ -30,19 +30,19 @@ function(input, output){
   })
   output$den2 <- renderPlot({
     d2 <- density(iris$Sepal.Length)
-    hist(iris$Sepal.Width, breaks = 12, prob = TRUE, xlab = "Sepal Length", main = "Sepal Length Histogram / Density")
-    lines(d1, lty = 3, col= "green")
+    hist(iris$Sepal.Length, breaks = 12, prob = TRUE, xlab = "Sepal Length", main = "Sepal Length Histogram / Density")
+    lines(d2, lty = 3, col= "green")
     abline(v=NP()$Sepal.Length, col = "red")
   })
   output$den3 <- renderPlot({
     d3 <- density(iris$Petal.Width)
     hist(iris$Petal.Width, breaks = 12, prob = TRUE, xlab = "Petal Width", main = "Petal Width Histogram / Density")
     lines(d3, lty = 3, col= "green")
-    abline(v=NP()$Sepal.Width, col = "red")
+    abline(v=NP()$Petal.Width, col = "red")
   })
   output$den4 <- renderPlot({
     d4 <- density(iris$Petal.Length)
-    hist(iris$Petal.Width, breaks = 12, prob = TRUE, xlab = "Petal Length", main = "Petal Length Histogram / Density")
+    hist(iris$Petal.Length, breaks = 12, prob = TRUE, xlab = "Petal Length", main = "Petal Length Histogram / Density")
     lines(d4, lty = 3, col= "green")
     abline(v=NP()$Petal.Length, col = "red")
   })
